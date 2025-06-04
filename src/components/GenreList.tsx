2d6ab61c0b34962a831040543a1b9df22e28c6e5
Meron <meronmuleta25@gmail.com>
2025-06-04 17:53:09 +0300

    Filter games by genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,20 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
-import useGenres from "../hooks/useGenres";
+import {
+  Button,
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Select,
+} from "@chakra-ui/react";
+import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 import GenreListSkeleton from "./GenreListSkeleton";
 
-const GenreList = () => {
+interface Props {
+  onSelectGenre: (genre: Genre) => void;
+}
+
+const GenreList = ({ onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
   if (error) return null;
@@ -11,8 +22,8 @@ const GenreList = () => {
     <List>
       {isLoading &&
         skeletons.map((skeleton) => (
-          <ListItem paddingY="5px">
-            <GenreListSkeleton key={skeleton} />
+          <ListItem key={skeleton} paddingY="5px">
+            <GenreListSkeleton />
           </ListItem>
         ))}
       {data.map((genre) => (
@@ -23,7 +34,13 @@ const GenreList = () => {
               borderRadius={8}
               src={getCroppedImageUrl(genre.image_background)}
             />
-            <Text fontSize="lg">{genre.name}</Text>
+            <Button
+              onClick={() => onSelectGenre(genre)}
+              fontSize="lg"
+              variant="link"
+            >
+              {genre.name}
+            </Button>
           </HStack>
         </ListItem>
       ))}
